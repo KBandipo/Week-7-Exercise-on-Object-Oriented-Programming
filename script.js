45a2245c5523fc886d68948eab22ebd3206ab5e5
kennybandy <nekbandy.com>
2023-09-07 23:25:09 +0100

    Define the Pseudocode

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 "use strict";
 /* Write a JavaScript program that creates a class called 'Animal' with properties for species and sound. Include a method to make the animal's sound. Create a subclass called 'Dog' that inherits from the 'Animal' class and adds an additional property for color. Override the make sound method to include the dog's color. Create an instance of the 'Dog' class and make it make its sound."*/
+//Pseudocode
+
+// Create class 'Animal'
+//Add Properties of 'species' and 'sound'
+//Include a method 'MakeSound'
+//Create a sub-class 'Dog' that inherit the properties of 'Animal'
+//Add addotional property of 'color' to the 'Dog' class properties
+//Override the Makesound method to include 'Dog' class
+//Make the 'Dog' sound
